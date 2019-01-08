36f77529d1fa6b4b98c67a0e9d56432e26aedb72
Niklas Brunlid <niklas.brunlid@sony.com>
2019-01-08 16:38:30 +0100

    Break out PluginFragment
    Break out generic PluginFragment from PluginSettingsFragment.

--- src/main/kotlin/com/prefect47/pluginlib/ui/settings/PluginSettingsFragment.kt
+++ src/main/kotlin/com/prefect47/pluginlib/ui/settings/PluginSettingsFragment.kt
@@ -2,28 +2,16 @@ package com.prefect47.pluginlib.ui.settings
 
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
-import com.prefect47.pluginlib.PluginLibrary
-import com.prefect47.pluginlib.plugin.PluginMetadata
 import com.prefect47.pluginlib.plugin.PluginSettings
+import com.prefect47.pluginlib.ui.PluginFragment
 
-class PluginSettingsFragment : Fragment() {
-    private lateinit var metadata: PluginMetadata
-
+class PluginSettingsFragment : PluginFragment() {
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val className = arguments!!.getString(PluginLibrary.ARG_CLASSNAME)
-        metadata = PluginLibrary.getMetaData(className!!)!!
-
         val layoutResId = (metadata.plugin as PluginSettings).layoutResId
         return inflater.inflate(layoutResId, container, false)
-    }
-
-    override fun onAttachFragment(childFragment: Fragment) {
-        childFragment.arguments = childFragment.arguments?.let { it.putAll(arguments); it } ?: arguments
-        super.onAttachFragment(childFragment)
     }
 }

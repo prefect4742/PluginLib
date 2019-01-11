a513aed5600f435ca9621bbd800f051b48953cea
prefect4742 <4999468+prefect4742@users.noreply.github.com>
2019-01-11 21:34:05 +0100

    Support inputType and digits attrs in text dialog
    Add support for the android:inputType and android:digits attributes
    on PluginEditTextPreference.

--- pluginlib/src/main/kotlin/com/prefect47/pluginlib/impl/ui/PluginEditTextPreferenceDialogFragment.kt
+++ pluginlib/src/main/kotlin/com/prefect47/pluginlib/impl/ui/PluginEditTextPreferenceDialogFragment.kt
@@ -1,7 +1,9 @@
 package com.prefect47.pluginlib.impl.ui
 
-import android.content.Context
 import android.os.Bundle
+import android.text.method.DigitsKeyListener
+import android.view.View
+import android.widget.EditText
 import androidx.preference.EditTextPreferenceDialogFragmentCompat
 import com.prefect47.pluginlib.PluginLibrary
 import com.prefect47.pluginlib.plugin.PluginMetadata
@@ -11,11 +13,17 @@ import com.prefect47.pluginlib.plugin.PluginMetadata
  */
 class PluginEditTextPreferenceDialogFragment : EditTextPreferenceDialogFragmentCompat() {
     companion object {
-        fun create(className: String, key: String): PluginEditTextPreferenceDialogFragment {
+        private const val ARG_INPUTTYPE = "inputType"
+        private const val ARG_DIGITS = "digits"
+
+        fun create(className: String, key: String, inputType: Int,
+                   digits: String?): PluginEditTextPreferenceDialogFragment {
             val fragment = PluginEditTextPreferenceDialogFragment()
             val b = Bundle(1)
             b.putString(ARG_KEY, key)
             b.putString(PluginLibrary.ARG_CLASSNAME, className)
+            b.putInt(ARG_INPUTTYPE, inputType)
+            digits?.let { b.putString(ARG_DIGITS, digits) }
             fragment.arguments = b
             return fragment
         }
@@ -27,5 +35,13 @@ class PluginEditTextPreferenceDialogFragment : EditTextPreferenceDialogFragmentC
         val className = arguments!!.getString(PluginLibrary.ARG_CLASSNAME)
         metadata = PluginLibrary.getMetaData(className!!)!!
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onBindDialogView(view: View?) {
+        view?.findViewById<EditText>(android.R.id.edit)?.apply {
+            inputType = arguments!!.getInt(ARG_INPUTTYPE)
+            arguments!!.getString(ARG_DIGITS)?.let { keyListener = DigitsKeyListener.getInstance(it) }
+        }
+        super.onBindDialogView(view)
     }
 }

b1cd46679ea8bad9c0f5eced89a7eb316f35f4fe
prefect4742 <4999468+prefect4742@users.noreply.github.com>
2019-01-12 00:46:47 +0100

    Notify Plugins when their settings changes
    Plugins that implement PluginSetting can choose to override
    onSharedPreferenceChanged.

--- pluginlib/src/main/kotlin/com/prefect47/pluginlib/plugin/PluginSettings.kt
+++ pluginlib/src/main/kotlin/com/prefect47/pluginlib/plugin/PluginSettings.kt
@@ -1,10 +1,11 @@
 package com.prefect47.pluginlib.plugin
 
+import android.content.SharedPreferences
 import com.prefect47.pluginlib.R
 import com.prefect47.pluginlib.plugin.annotations.ProvidesInterface
 
 @ProvidesInterface(version = PluginSettings.VERSION)
-interface PluginSettings {
+interface PluginSettings: SharedPreferences.OnSharedPreferenceChangeListener {
 
     companion object {
         const val VERSION = 1
@@ -14,4 +15,6 @@ interface PluginSettings {
         get() = R.layout.plugin_settings_fragment
 
     val preferencesResId: Int
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {}
 }

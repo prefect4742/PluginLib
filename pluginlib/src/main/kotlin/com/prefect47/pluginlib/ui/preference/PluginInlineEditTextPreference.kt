c40e8f4f5660b10159a7df9fae5041d962c6297a
prefect4742 <4999468+prefect4742@users.noreply.github.com>
2019-01-18 21:43:03 +0100

    Final touch on inline text editor
    Text value is now persisted, keyboard stays down on rotation.
    Only remaining issue is that the first EditText on screen will gain focus
    on an orientation change (screen rotation) since all EditText fields share
    the same ID.
    
    Issue: #2

--- pluginlib/src/main/kotlin/com/prefect47/pluginlib/ui/preference/PluginInlineEditTextPreference.kt
+++ pluginlib/src/main/kotlin/com/prefect47/pluginlib/ui/preference/PluginInlineEditTextPreference.kt
@@ -1,6 +1,9 @@
 package com.prefect47.pluginlib.ui.preference
 
 import android.content.Context
+import android.content.res.TypedArray
+import android.os.Parcel
+import android.os.Parcelable
 import android.text.InputType
 import android.text.method.DigitsKeyListener
 import android.util.AttributeSet
@@ -8,6 +11,8 @@ import androidx.appcompat.widget.AppCompatEditText
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 import com.prefect47.pluginlib.R
+import com.prefect47.pluginlib.impl.extensions.afterTextChanged
+import com.prefect47.pluginlib.impl.extensions.createParcel
 
 /**
  * Inline EditText Preference.
@@ -16,10 +21,11 @@ class PluginInlineEditTextPreference @JvmOverloads constructor(context: Context,
         defStyleAttr: Int = android.R.attr.editTextPreferenceStyle,
         defStyleRes: Int = android.R.attr.editTextPreferenceStyle)
             : Preference(context, attrs, defStyleAttr, defStyleRes) {
-    var titleAttr: String? = null
+    var editText: AppCompatEditText? = null
+    var value: String? = null
+
     var inputTypeAttr: Int = InputType.TYPE_CLASS_TEXT
     var digitsAttr: String? = null
-    var defaultValueAttr: String? = null
     var hintAttr: String? = null
 
     init {
@@ -27,11 +33,9 @@ class PluginInlineEditTextPreference @JvmOverloads constructor(context: Context,
             context.theme.obtainStyledAttributes(attrs, R.styleable.PluginInlineEditTextPreference,
                     defStyleAttr, defStyleRes).apply {
                 try {
-                    titleAttr = getString(R.styleable.PluginInlineEditTextPreference_android_title)
                     inputTypeAttr = getInt(R.styleable.PluginInlineEditTextPreference_android_inputType,
                         InputType.TYPE_CLASS_TEXT)
                     digitsAttr = getString(R.styleable.PluginInlineEditTextPreference_android_digits)
-                    defaultValueAttr = getString(R.styleable.PluginInlineEditTextPreference_android_defaultValue)
                     hintAttr = getString(R.styleable.PluginInlineEditTextPreference_android_hint)
                 } finally {
                     recycle()
@@ -40,17 +44,92 @@ class PluginInlineEditTextPreference @JvmOverloads constructor(context: Context,
         }
 
         layoutResource = R.layout.plugin_pref_inline_edittext
-        //widgetLayoutResource = R.layout.plugin_pref_widget_edittext
     }
 
     override fun onBindViewHolder(holder: PreferenceViewHolder?) {
         super.onBindViewHolder(holder)
+        editText = (holder?.findViewById(android.R.id.edit) as? AppCompatEditText) ?:
+            throw IllegalStateException("PluginInlineEditTextPreference layout must contain an AppCompatEditText with "
+                    + "id @android:id/edit")
 
-        (holder?.findViewById(R.id.plugin_pref_inline_edittext_value) as? AppCompatEditText)?.apply {
+        editText?.apply {
             inputType = inputTypeAttr
             digitsAttr?.let { keyListener = DigitsKeyListener.getInstance(it) }
-            defaultValueAttr?.let { setText(it) }
             hintAttr?.let { hint = hintAttr }
+            setText(value)
+            afterTextChanged {
+                if (callChangeListener(it)) {
+                    setTextInternal(it, false)
+                } else {
+                    editText?.setText(it)
+                }
+            }
+        }
+        editText?.isEnabled = isEnabled
+    }
+
+    override fun onSetInitialValue(defaultValue: Any?) {
+        setText(getPersistedString(defaultValue as? String))
+    }
+
+    override fun onGetDefaultValue(a: TypedArray, index: Int): Any {
+        return a.getString(index) as Any
+    }
+
+    fun setText(text: String?) {
+        setTextInternal(text, true)
+    }
+
+    private fun setTextInternal(text: String?, notifyChanged: Boolean) {
+        value = text
+        persistString(value)
+        if (notifyChanged) {
+            notifyChanged()
+        }
+    }
+
+    fun getText(): String? = value
+
+    override fun onSaveInstanceState(): Parcelable {
+        val superState = super.onSaveInstanceState()
+        if (isPersistent) {
+            // No need to save instance state since it's persistent
+            return superState
+        }
+        val ss = SavedState(superState)
+        ss.text = value
+        return ss
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable) {
+        if (state !is SavedState) {
+            super.onRestoreInstanceState(state)
+            return
+        }
+
+        super.onRestoreInstanceState(state.superState)
+        value = state.text
+        notifyChanged()
+    }
+
+    class SavedState: BaseSavedState {
+        companion object {
+            @JvmField @Suppress("unused")
+            val CREATOR = createParcel { SavedState(it) }
+        }
+
+        var text: String? = null
+
+        @Suppress("unused")
+        constructor(superState: Parcelable) : super(superState)
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            super.writeToParcel(dest, flags)
+            dest.writeString(text)
+        }
+
+        private constructor(source: Parcel) : super(source) {
+            text = source.readString()
         }
     }
 }
--- pluginlib/src/main/kotlin/com/prefect47/pluginlib/ui/preference/PluginPreference.kt
+++ pluginlib/src/main/kotlin/com/prefect47/pluginlib/ui/preference/PluginPreference.kt
@@ -1,20 +1,17 @@
 package com.prefect47.pluginlib.ui.preference
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import androidx.preference.Preference
 
 /**
  * Preference category that automatically adds all plugins of its type and sets their common layout.
  */
-abstract class PluginPreference @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null,
+class PluginPreference @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null,
         defStyleAttr: Int = android.R.attr.preferenceStyle,
         defStyleRes: Int = android.R.attr.preferenceStyle)
             : Preference(context, attrs, defStyleAttr, defStyleRes) {
 
-    init {
-        init(attrs, defStyleAttr, defStyleRes)
-    }
-
-    protected abstract fun init(attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int)
 }

5535b506bde4035fad078509254feccfe9301b04
prefect4742 <4999468+prefect4742@users.noreply.github.com>
2019-04-02 16:10:50 +0200

    Trying stuff with layout inflaters

--- pluginlib/src/main/kotlin/com/prefect47/pluginlib/impl/DiscoverableContextWrapper.kt
+++ pluginlib/src/main/kotlin/com/prefect47/pluginlib/impl/DiscoverableContextWrapper.kt
@@ -18,13 +18,86 @@ package com.prefect47.pluginlib.impl
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.content.res.AssetManager
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatViewInflater
+import androidx.core.view.LayoutInflaterCompat
+import kotlin.reflect.full.declaredFunctions
 
 class DiscoverableContextWrapper(private val appContext: Context, base: Context, private val myClassLoader: ClassLoader,
                                  internal val pkg: String) : ContextWrapper(base) {
+
+    inner class MyRes: Resources(myAssetManager, appContext.resources.displayMetrics, appContext.resources.configuration) {
+        override fun getDrawable(id: Int): Drawable {
+            return super.getDrawable(id)
+        }
+
+        override fun getDrawable(id: Int, theme: Theme?): Drawable {
+            return super.getDrawable(id, theme)
+        }
+
+        override fun getDrawableForDensity(id: Int, density: Int): Drawable? {
+            return super.getDrawableForDensity(id, density)
+        }
+
+        override fun getDrawableForDensity(id: Int, density: Int, theme: Theme?): Drawable? {
+            return super.getDrawableForDensity(id, density, theme)
+        }
+    }
+
     private val inflater: LayoutInflater by lazy {
-        LayoutInflater.from(appContext).cloneInContext(this)
+        //AppCompatViewInflater().
+        //LayoutInflater.from(appContext).cloneInContext(this)
+        /*
+        val layoutInflater = DiscoverableLayoutInflater(this)
+        LayoutInflaterCompat.setFactory2(layoutInflater, DiscoverableLayoutInflaterFactory(this))
+        layoutInflater
+        */
+        DiscoverableLayoutInflater(LayoutInflater.from(appContext), appContext, this)
+    }
+
+    private val myAssetManager: AssetManager by lazy {
+        val assetManager = AssetManager::class.java.newInstance()
+        val addAssetPath = assetManager::class.java.getMethod("addAssetPath", String::class.java)
+        val apkPath = packageResourcePath
+        addAssetPath.invoke(assetManager, apkPath)
+        assetManager
+    }
+
+    private val myResources: Resources by lazy {
+        MyRes()
+        /*
+        val baseResources = appContext.resources
+        Resources(myAssetManager, baseResources.displayMetrics, baseResources.configuration)
+        */
+
+        /*
+        public PluginContext(Context baseContext, String apkPath, final DexClassLoader dexClassLoader) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        super(baseContext);
+        this.dexClassLoader = dexClassLoader;
+        assetManager = AssetManager.class.newInstance();
+        Method addAssetPath = assetManager.getClass().getMethod("addAssetPath", String.class);
+        addAssetPath.invoke(assetManager, apkPath);
+
+        Resources baseResources = baseContext.getResources();
+        resources = new Resources(assetManager, baseResources.getDisplayMetrics(), baseResources.getConfiguration());
+        theme = resources.newTheme();
+        theme.setTo(getTheme());
+
+        layoutInflater = new PluginLayoutInflater(LayoutInflater.from(baseContext), this);
+
+
+
+        res
+        */
+        /*
+        val x = packageManager.getResourcesForApplication(pkg)
+        x
+        */
     }
 
     override fun getClassLoader(): ClassLoader {
@@ -40,6 +113,14 @@ class DiscoverableContextWrapper(private val appContext: Context, base: Context,
             return inflater
         }
         return baseContext.getSystemService(name) as Any
+    }
+
+    override fun getResources(): Resources {
+        return myResources
+    }
+
+    override fun getAssets(): AssetManager {
+        return myAssetManager
     }
 
     override fun getSharedPreferences(name: String?, mode: Int): SharedPreferences? {
---
+++ pluginlib/src/main/kotlin/com/prefect47/pluginlib/impl/DiscoverableLayoutInflater.kt
@@ -0,0 +1,129 @@
+package com.prefect47.pluginlib.impl
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.LayoutInflaterCompat
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.annotation.Nullable
+import org.xmlpull.v1.XmlPullParser
+import java.lang.reflect.Constructor
+
+class DiscoverableLayoutInflater(
+    original: LayoutInflater, private val appContext: Context, private val dContext: Context
+): LayoutInflater(dContext), LayoutInflater.Factory2 {
+
+    override fun cloneInContext(newContext: Context): LayoutInflater {
+        return DiscoverableLayoutInflater(this, appContext, newContext)
+    }
+
+    override fun getContext(): Context {
+        return dContext
+    }
+
+    override fun inflate(parser: XmlPullParser?, root: ViewGroup?): View {
+        return super.inflate(parser, root)
+    }
+
+    override fun inflate(resource: Int, root: ViewGroup?, attachToRoot: Boolean): View {
+        return super.inflate(resource, root, attachToRoot)
+    }
+
+    override fun inflate(@LayoutRes resource: Int, @Nullable root: ViewGroup?): View {
+        return super.inflate(resource, root)
+    }
+
+    override fun inflate(parser: XmlPullParser?, root: ViewGroup?, attachToRoot: Boolean): View {
+        return super.inflate(parser, root, attachToRoot)
+    }
+
+    /*
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return onCreateView(null, name, context, attrs)
+
+        /*
+        return if (name ==  "TextView") {
+            AppCompatTextView(context, attrs)
+        } else if (name == "ImageView") {
+            AppCompatImageView(context, attrs)
+        }
+        else null
+        /*
+        var constructor: Constructor<out View>? = null
+        var clazz: Class<out View>? = null
+        try {
+            clazz = contextWrapper.classLoader.loadClass(name).asSubclass<View>(View::class.java)
+            constructor = clazz!!.getConstructor(*constructorSignature)
+            constructor!!.setAccessible(true)
+            return constructor.newInstance(contextWrapper, attrs)
+        } catch (t: Throwable) {
+            return null
+        }
+        */
+        * */
+    }
+    */
+
+    override fun onCreateView(name: String?, attrs: AttributeSet?): View? {
+        return onCreateView(null, name, null, attrs)
+    }
+
+    override fun onCreateView(parent: View?, name: String?, attrs: AttributeSet?): View? {
+        return onCreateView(parent, name, dContext, attrs)
+    }
+
+    override fun onCreateView(name: String?, context: Context?, attrs: AttributeSet?): View? {
+        return onCreateView(null, name, context, attrs)
+    }
+
+    override fun onCreateView(parent: View?, name: String?, context: Context?, attrs: AttributeSet?): View? {
+        var constructor: Constructor<out View>? = null
+        var clazz: Class<out View>? = null
+
+        if (clazz == null) {
+            try {
+                clazz = dContext.classLoader.loadClass(name).asSubclass<View>(View::class.java)
+            } catch (e: ClassNotFoundException) {
+            }
+        }
+
+        if (clazz == null) {
+            try {
+                clazz = dContext.classLoader.loadClass("android.widget.$name").asSubclass<View>(View::class.java)
+            } catch (e: ClassNotFoundException) {
+            }
+        }
+
+        if (clazz == null) {
+            try {
+                clazz = appContext.classLoader.loadClass(name).asSubclass<View>(View::class.java)
+            } catch (e: ClassNotFoundException) {
+            }
+        }
+
+        if (clazz == null) {
+            try {
+                clazz = appContext.classLoader.loadClass("android.widget.$name").asSubclass<View>(View::class.java)
+            } catch (e: ClassNotFoundException) {
+            }
+        }
+
+        return try {
+            constructor = clazz!!.getConstructor(*constructorSignature)
+            constructor!!.isAccessible = true
+            constructor.newInstance(context, attrs)
+        } catch (t: Throwable) {
+            null
+        }
+    }
+
+    companion object {
+
+        private val constructorSignature = arrayOf<Class<*>>(Context::class.java, AttributeSet::class.java)
+    }
+}
